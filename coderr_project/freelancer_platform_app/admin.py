18997ba37584861fa171a1687ac27cd5c785e09b
Bladers123 <tristangehring123@hotmail.de>
2025-01-11 16:03:46 +0100

    testing

--- coderr_project/freelancer_platform_app/admin.py
+++ coderr_project/freelancer_platform_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Offer, Order, Profile, BaseInfo, BusinessProfile, CustomerProfile, Review, FileUpload, OfferDetail
 
-# Register your models here.
+
+
+admin.site.register(Offer)
+admin.site.register(Order)
+admin.site.register(Profile)
+admin.site.register(BaseInfo)
+admin.site.register(BusinessProfile)
+admin.site.register(CustomerProfile)
+admin.site.register(Review)
+admin.site.register(FileUpload)
+admin.site.register(OfferDetail)
+
+
