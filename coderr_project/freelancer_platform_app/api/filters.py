752bf37b3ae75d1d4706b98d51899a21a1bd4757
Bladers123 <tristangehring123@hotmail.de>
2025-01-18 14:19:56 +0100

    search by title

--- coderr_project/freelancer_platform_app/api/filters.py
+++ coderr_project/freelancer_platform_app/api/filters.py
@@ -8,7 +8,7 @@ class OfferFilter(filters.FilterSet):
     creator_id = filters.NumberFilter(field_name='user__id', lookup_expr='exact')  # Benutzer-ID (indirekt über user)
     min_price = filters.NumberFilter(field_name='min_price', lookup_expr='gte')  # Mindestpreis
     max_delivery_time = filters.NumberFilter(field_name='min_delivery_time', lookup_expr='lte')  # minimale Lieferzeit
-    search = filters.CharFilter(method='filter_search')
+    search = filters.CharFilter(field_name='title', lookup_expr='icontains')  # Sucht nach dem Titel
 
     class Meta:
         model = Offer
