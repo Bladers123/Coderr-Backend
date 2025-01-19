700eca380ea036539dcf827475010252cb24b9fe
Bladers123 <tristangehring123@hotmail.de>
2025-01-19 14:51:38 +0100

    updates

--- coderr_project/freelancer_platform_app/api/serializers.py
+++ coderr_project/freelancer_platform_app/api/serializers.py
@@ -124,7 +124,7 @@ class FileUploadSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ['id', 'user', 'customer_user', 'business_user', 'title', 'revisions', 'delivery_time_in_days', 'price', 'features', 'offer_type', 'status', 'created_at', 'updated_at']
+        fields = ['id', 'customer_user', 'business_user', 'title', 'revisions', 'delivery_time_in_days', 'price', 'features', 'offer_type', 'status', 'created_at', 'updated_at']
 
 
 
